23d7fb3a032fd688a5a35f11649859bdba788b0f
NotPeOpLe <code840@outlook.com>
2025-02-22 20:38:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# catJAM for tosu
+
+一個 catJAM 動畫，會隨著 BPM 調整點頭速度，好玩的
+
+動畫使用 [gak](https://gka.js.org/) 生成，改寫一下 JS 代碼
+
+## 已知問題
+
+# catJAM for tosu
+
+A catJAM animation that adjusts its nodding speed according to the BPM, fun to watch.
+
+The animation is generated using [gak](https://gka.js.org/) with some modifications to the JS code.
+
+## Known Issues
+
+- The nodding is not synchronized with the beat (but I don't know how to fix it)
---
+++ index.js
@@ -0,0 +1,27 @@
+let realtimeBPM;
+let currentBeatmap;
+let catJAMEl = document.getElementById("catJAM");
+let catJAM = catJAMEl.getAnimations()[0];
+let ws;
+
+const app = () => {
+  ws = new WebSocket("ws://127.0.0.1:24050/websocket/v2");
+
+  // connection failed retry
+  ws.onerror = app;
+
+  ws.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    if (realtimeBPM !== data.beatmap.stats.bpm.realtime) {
+      realtimeBPM = data.beatmap.stats.bpm.realtime;
+      catJAM.cancel(); // reset animation
+      catJAM.updatePlaybackRate(realtimeBPM / 124);
+      catJAM.play();
+    }
+  };
+};
+
+document.addEventListener("animationstart", () => {
+  catJAM = catJAMEl.getAnimations()[0];
+  app();
+});
